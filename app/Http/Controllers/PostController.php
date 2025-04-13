c0619f635fca2afb7e0ae3087e2c466aa34fc834
KacperWalenga <kacper.walenga@studenci.collegiumwitelona.pl>
2025-04-13 15:24:24 +0000

    Sorting posts by creation date

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,7 +28,7 @@ class PostController extends Controller
 
     public function index(): JsonResponse
     {
-        $posts = Post::query()->with(["user", "tag", "game"])->paginate(10);
+        $posts = Post::query()->with(["user", "tag", "game"])->orderBy('created_at', 'desc')->paginate(10);
 
         return response()->json($posts);
     }
