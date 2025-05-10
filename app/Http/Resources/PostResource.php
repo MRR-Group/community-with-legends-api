1515a9e19ccea8a6b754ba0e59d7b6d294bc6d56
Dominik Purgał <amonsir29@gmail.com>
2025-05-10 22:12:53 +0200

    - Add resources (#75)
    * Add resources
    
    * Fix code style
    
    * convert response()->json to Resource
    
    * change BelongsTo to lowercase
    
    * fix doc comment

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommunityWithLegends\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "content" => $this->content,
+            "created_at" => $this->created_at,
+            "user" => UserResource::make($this->user),
+            "game" => GameResource::make($this->game),
+            "tags" => $this->tags->pluck("name"),
+            "asset" => PostAssetResource::make($this->asset),
+            "reactions" => $this->reactions->count(),
+            "user_reacted" => $this->reactions->contains("user_id", $request->user()->id),
+            "comments" => CommentResource::collection($this->comments),
+        ];
+    }
+}
