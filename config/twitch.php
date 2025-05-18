ed50c467929931838cc66143770f6849b36a6c7e
Kacper Walenga <87613926+KacperWalenga@users.noreply.github.com>
2025-05-18 21:31:12 +0200

    fix twitch auth (#86)

--- config/twitch.php
+++ config/twitch.php
@@ -7,6 +7,8 @@ return [
     "client_secret" => env("TWITCH_CLIENT_SECRET"),
     "code_redirect_uri" => env("TWITCH_CODE_REDIRECT_URL"),
     "token_redirect_uri" => env("TWITCH_TOKEN_REDIRECT_URL"),
-    "twitch_login_redirect_url_mobile" => env("TWITCH_LOGIN_REDIRECT_URL_MOBILE"),
-    "twitch_login_redirect_url_web" => env("TWITCH_LOGIN_REDIRECT_URL_WEB"),
+    "login_redirect_url_mobile" => env("TWITCH_LOGIN_REDIRECT_URL_MOBILE"),
+    "login_redirect_url_web" => env("TWITCH_LOGIN_REDIRECT_URL_WEB"),
+    "login_redirect_error_url_mobile" => env("TWITCH_LOGIN_ERROR_REDIRECT_URL_MOBILE"),
+    "login_redirect_error_url_web" => env("TWITCH_LOGIN_ERROR_REDIRECT_URL_WEB"),
 ];
