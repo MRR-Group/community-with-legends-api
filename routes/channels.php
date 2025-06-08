9dc263ca778d5b8038e5c43f3a1ad8e9bf27ad1b
Oliwia-Charyszczak <oliwia.charyszczak@studenci.collegiumwitelona.pl>
2025-06-08 05:49:15 +0200

    Add role-based access to channels

--- routes/channels.php
+++ routes/channels.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
+use CommunityWithLegends\Enums\Role;
 use CommunityWithLegends\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel("user.{id}", fn(User $user, mixed $id) => true);
-Broadcast::channel("admin", fn(User $user) => true);
+Broadcast::channel("user.{id}", fn(User $user, mixed $id) => $user->id === (int)$id);
+Broadcast::channel("admin", fn(User $user) => $user->hasAnyRole([Role::SuperAdministrator, Role::Administrator]));
